be7dadeda01de07d0d130146e7ce5c056ec6168a
Raissa Cavalcanti <168494363+Raissa2308@users.noreply.github.com>
2024-06-05 21:41:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
-# Definindo a variável de ambiente para modo de desenvolvimento debug
+
 os.environ['FLASK_DEBUG'] = 'True'
 app.debug = os.environ.get('FLASK_DEBUG') == 'True'
 
@@ -14,67 +14,34 @@ def pagina_home():
     return render_template('index.html')
 
 
-# Exemplo de rota para a página sobre
+
 @app.route('/sobre')
 def pagina_sobre():
     return render_template('sobre.html')
 
+@app.route('/atividades')
+def atividades():
+    return render_template('atividades.html')
 
-# Exemplo de rota para a página de glossário
-@app.route('/glossario')
-def glossario():
+
+
+@app.route('/cursos')
+def cursos():
 
     glossario_de_termos = []
 
     with open(
-            'bd_glossario.csv',
+            'bd_cursos.csv',
             newline='', encoding='utf-8') as arquivo:
         reader = csv.reader(arquivo, delimiter=';')
         for l in reader:
             glossario_de_termos.append(l)
 
-    return render_template('glossario.html',
-                           glossario=glossario_de_termos)
+    return render_template('cursos.html',
+                           cursos=glossario_de_termos)
 
 
-@app.route('/novo_termo')
-def novo_termo():
-    return render_template('adicionar_termo.html')
 
-
-@app.route('/criar_termo', methods=['POST', ])
-def criar_termo():
-    termo = request.form['termo']
-    definicao = request.form['definicao']
-
-    with open(
-            'bd_glossario.csv', 'a',
-            newline='', encoding='utf-8') as arquivo:
-        writer = csv.writer(arquivo, delimiter=';')
-        writer.writerow([termo, definicao])
-
-    return redirect(url_for('glossario'))
-
-
-@app.route('/excluir_termo/<int:termo_id>', methods=['POST'])
-def excluir_termo(termo_id):
-
-    with open('bd_glossario.csv', 'r', newline='') as file:
-        reader = csv.reader(file)
-        linhas = list(reader)
-
-    # Encontrar e excluir o termo com base no ID
-    for i, linha in enumerate(linhas):
-        if i == termo_id:
-            del linhas[i]
-            break
-
-    # Salvar as alterações de volta no arquivo
-    with open('bd_glossario.csv', 'w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerows(linhas)
-
-    return redirect(url_for('glossario'))
 
 
 
